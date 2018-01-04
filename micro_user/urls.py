3e023238e3cb832ed0e2f9ff4c7c2b88893ad399
Paula Crismaru <paulacrismaru@gmail.com>
2018-01-04 21:49:47 +0200

    Fix urls

--- micro_user/urls.py
+++ micro_user/urls.py
@@ -2,18 +2,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from friends.urls import friends_urls
+from groups.urls import groups_urls
 from users.urls import user_urls
-
-from users import views as views_users
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^', include('groups.urls')),
-    # TODO: home
-    url(r'^$', views_users.CurrentUserDetails.as_view())
 ]
 
 urlpatterns.extend(user_urls)
 urlpatterns.extend(friends_urls)
+urlpatterns.extend(groups_urls)
